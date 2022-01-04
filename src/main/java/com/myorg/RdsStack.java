aa8041452acacb4631c7b9409c19de96b60e5e09
Everton Frozza <evertonfrozza@gmail.com>
2022-01-04 18:24:08 -0300

    criacao da stack RDS

---
+++ src/main/java/com/myorg/RdsStack.java
@@ -0,0 +1,59 @@
+package com.myorg;
+
+import software.amazon.awscdk.CfnParameter;
+import software.amazon.awscdk.SecretValue;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.*;
+import software.constructs.Construct;
+
+import java.util.Collections;
+
+public class RdsStack extends Stack {
+    public RdsStack(final Construct scope, final String id, Vpc vpc) {
+
+        this(scope, id, null, vpc);
+    }
+
+    public RdsStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
+        super(scope, id, props);
+
+        // usuario e senha do DB
+
+        CfnParameter databasePassword = CfnParameter.Builder.create(this, "databasePassword")
+                .type("String")
+                .description("The RDS Instance password")
+                .build();
+
+        // Porta de acesso
+
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+
+        // criar instancia RDS
+
+        DatabaseInstance databaseInstance = DatabaseInstance.Builder
+                .create(this, "Rds01")
+                .instanceIdentifier("aws-project01-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_5_7)
+                        .build()))
+                .vpc(vpc)
+                .credentials(Credentials.fromUsername("admin",
+                        CredentialsFromUsernameOptions.builder()
+                .password(SecretValue.plainText(databasePassword.getValueAsString()))
+                .build()))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+
+
+
+    }
+}
